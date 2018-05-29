a6c8fd8607ce5efbb84b0763135081c86f0abcbd
David Toca <david.virusfel@gmail.com>
2018-05-28 23:08:34 -0500

    using template handler as function

--- main.go
+++ main.go
@@ -3,7 +3,27 @@ package main
 import (
   "log"
   "net/http"
+  "text/template"
+  "path/filepath"
+  "sync"
 )
+
+type templateHandler struct {
+  once sync.Once
+  filename string
+  templ *template.Template
+}
+
+func (handler *templateHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
+  handler.once.Do (func(){
+      handler.templ = template.Must(
+        template.ParseFiles(
+            filepath.Join("templates", handler.filename),
+        ),
+      )
+  })
+  handler.templ.Execute(w, nil)
+}
 
 func render(w http.ResponseWriter, r *http.Request) {
   w.Write([]byte(`
@@ -16,7 +36,8 @@ func render(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-  http.HandleFunc("/", render)
+  handler := templateHandler{filename: "chat.html"}
+  handler.HandleFunc("/", handler.ServeHTTP)
   // start web server
 
   if err := http.ListenAndServe(":8000", nil); err != nil{
