5a5948394d0a3c7a77a49ae4f599a97aefdfd020
David Toca <david.virusfel@gmail.com>
2018-05-28 23:29:44 -0500

    refactoring

--- main.go
+++ main.go
@@ -3,40 +3,12 @@ package main
 import (
   "log"
   "net/http"
-  "text/template"
-  "path/filepath"
-  "sync"
+  "github.com/DavidToca/simplechat/templatehandler"
 )
 
-type templateHandler struct {
-  once sync.Once
-  filename string
-  templ *template.Template
-}
-
-func (handler *templateHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
-  handler.once.Do (func(){
-      handler.templ = template.Must(
-        template.ParseFiles(
-            filepath.Join("templates", handler.filename),
-        ),
-      )
-  })
-  handler.templ.Execute(w, nil)
-}
-
-func render(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte(`
-    <html>
-    <body>
-      <h1>Chat Test</h1>
-    </body>
-    </html>
-  `))
-}
 
 func main() {
-  handler := &templateHandler{filename: "chat.html"}
+  handler := &templatehandler.TemplateHandler{Filename: "chat.html"}
   http.Handle("/", handler)
   // start web server
   if err := http.ListenAndServe(":8000", nil); err != nil{
---
+++ templatehandler/templatehandler.go
@@ -0,0 +1,25 @@
+package templatehandler
+
+import (
+  "net/http"
+  "text/template"
+  "path/filepath"
+  "sync"
+)
+
+type TemplateHandler struct {
+  once sync.Once
+  Filename string
+  templ *template.Template
+}
+
+func (handler *TemplateHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
+  handler.once.Do (func(){
+      handler.templ = template.Must(
+        template.ParseFiles(
+            filepath.Join("templates", handler.Filename),
+        ),
+      )
+  })
+  handler.templ.Execute(w, nil)
+}
