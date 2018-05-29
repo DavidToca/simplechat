3b91b74eb55646fa379e202b957fb2cf56c712f1
David Toca <david.virusfel@gmail.com>
2018-05-28 22:50:29 -0500

    decoupling anonymous function

--- main.go
+++ main.go
@@ -5,18 +5,18 @@ import (
   "net/http"
 )
 
+func render(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte(`
+    <html>
+    <body>
+      <h1>Chat Test</h1>
+    </body>
+    </html>
+  `))
+}
 
 func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-    w.Write([]byte(`
-      <html>
-      <body>
-        <h1>Chat Test</h1>
-      </body>
-      </html>
-    `))
-  })
-
+  http.HandleFunc("/", render)
   // start web server
 
   if err := http.ListenAndServe(":8000", nil); err != nil{
